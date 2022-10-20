998cacb9195869c7e49a927c0deaeb9170ce92b2
Mthunzi <mthunzi99@gmail.com>
2022-10-20 23:08:12 +0200

    Creates Polytree class

--- mthunzi_chikhungu_polytree/skeleton/lib/00_tree_node.rb
+++ mthunzi_chikhungu_polytree/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,31 @@
 class PolyTreeNode
+    attr_reader :parent, :children
+    attr_accessor :value
+
+    def initialize(value)
+        @parent = nil
+        @children = []
+        @value = value
+    end
+
+    def parent=(parent)
+        return if @parent == parent
+
+        @parent.children.delete(self) if @parent
+
+        @parent = parent
+        @parent.children << self unless @parent.nil?
+
+        self
+    end
+
+    def add_child(child)
+        child.parent = self
+    end
+
+    def remove_child(child)
+        raise "Node is not a valid child" if !@children.include?(child)
+        child.parent = nil
+    end
 
 end
