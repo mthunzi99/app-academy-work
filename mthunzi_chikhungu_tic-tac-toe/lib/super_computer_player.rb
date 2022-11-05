90d70e98d014bdab0d7de1a9836a941edbb7a210
Mthunzi <mthunzi99@gmail.com>
2022-11-05 20:55:09 +0200

    Completed super_computer subclass

--- mthunzi_chikhungu_tic-tac-toe/lib/super_computer_player.rb
+++ mthunzi_chikhungu_tic-tac-toe/lib/super_computer_player.rb
@@ -2,6 +2,17 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+
+    possible_moves = node.children
+
+    node = possible_moves.find { |child| child.winning_node?(mark) }
+    return node.prev_move_pos if node
+
+    node = possible_moves.find { |child| !child.losing_node?(mark) }
+    return node.prev_move_pos if node
+
+    raise "I'm not feeling so good"
   end
 end
 
