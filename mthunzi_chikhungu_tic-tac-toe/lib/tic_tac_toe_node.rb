19008fe6ae279927d6acd5c9d443d07e4a133ef6
Mthunzi <mthunzi99@gmail.com>
2022-11-05 17:47:39 +0200

    Completed losing_node? method

--- mthunzi_chikhungu_tic-tac-toe/lib/tic_tac_toe_node.rb
+++ mthunzi_chikhungu_tic-tac-toe/lib/tic_tac_toe_node.rb
@@ -1,10 +1,20 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board, @next_mover_mark, @prev_move_pos = board, next_mover_mark, prev_move_pos
   end
 
   def losing_node?(evaluator)
+    return @board.won? && @board.winner != evaluator if @board.over?
+
+    if @next_mover_mark == evaluator
+      children.all? { |node| node.losing_node?(evaluator) }
+    else
+      children.any? { |node| node.losing_node?(evaluator) }
+    end
   end
 
   def winning_node?(evaluator)
@@ -13,5 +23,20 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    nodes = []
+
+    (0...3).each do |x|
+      (0...3).each do |y|
+        pos = [x,y]
+        next unless @board.empty?(pos) # Skips populated positions
+
+        next_board = @board.dup
+        next_board[pos] = @next_mover_mark
+        next_mover_mark = (@next_mover_mark == :x ? :o : :x)
+        nodes << TicTacToeNode.new(next_board, next_mover_mark, pos)
+      end
+    end
+
+    nodes
   end
 end
