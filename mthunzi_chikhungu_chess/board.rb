5f2bc545299655cdb7837123ba037fae0b29c819
Mthunzi <mthunzi99@gmail.com>
2022-11-14 23:43:09 +0200

    Starting Phase 1

---
+++ mthunzi_chikhungu_chess/board.rb
@@ -0,0 +1,9 @@
+require_relative 'piece'
+
+class Board
+
+    def initialize
+
+    end
+    
+end
---
+++ mthunzi_chikhungu_chess/null_piece.rb
@@ -0,0 +1,9 @@
+require_relative 'piece'
+
+class NullPiece
+
+    def initialize
+
+    end
+    
+end
