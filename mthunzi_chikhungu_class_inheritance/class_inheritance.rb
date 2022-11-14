0f8c60bb317b3d4c0e064309a7bbcd7d5e2341fe
Mthunzi <mthunzi99@gmail.com>
2022-11-14 22:24:31 +0200

    Completed Manager class

--- mthunzi_chikhungu_class_inheritance/class_inheritance.rb
+++ mthunzi_chikhungu_class_inheritance/class_inheritance.rb
@@ -1,22 +1,56 @@
 class Employee
-    attr_reader :name, :title, :boss
-    attr_writer :salary
+    attr_reader :name, :title, :salary
+    attr_accessor :boss
 
-    def initialize(name, title, salary, boss = nil)
-        @name, @title, @salary, @boss = name, title, salary, boss
+    def initialize(name, title, salary, boss)
+        @name, @title, @salary = name, title, salary
+        self.boss = boss
     end
 
-    def salary
-        "$#{@salary}"
+    def boss=(boss)
+        @boss = boss
+        boss.add_subordinate(self) unless boss.nil?
     end
 
+    def bonus(multiplier)
+        @salary * multiplier
+    end
 end
 
 class Manager < Employee
+    attr_reader :subordinates
+
+    def initialize(name, title, salary, boss = nil)
+        super(name, title, salary, boss)
+        @subordinates = []
+    end
+
+    def add_subordinate(employee)
+        @subordinates << employee
+    end
+
+    def bonus(multiplier)
+        total_bonus = 0
+
+        @subordinates.each do |employee|
+            if employee.is_a?(Manager)
+                total_bonus += (employee.salary + employee.bonus(1))
+           else
+                total_bonus += employee.salary
+           end
+        end
+
+        total_bonus *= multiplier
+    end
 
 end
 
 
-# ned.bonus(5) # => 500_000
-# darren.bonus(4) # => 88_000
-# david.bonus(3) # => 30_000
+ned = Manager.new("Ned", "Founder", 1000000)
+darren = Manager.new("Darren", "TA Manager", 78000, ned)
+shawna = Employee.new("Shawna", "TA", 12000, darren)
+david = Employee.new("David", "TA", 10000, darren)
+
+puts ned.bonus(5) # => 500_000
+puts darren.bonus(4) # => 88_000
+puts david.bonus(3) # => 30_000
