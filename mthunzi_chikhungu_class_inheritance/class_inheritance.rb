2c857c5bd772d52a7d88faaece3933ddca3c31f2
Mthunzi <mthunzi99@gmail.com>
2022-11-14 21:10:50 +0200

    Employee class complete

--- mthunzi_chikhungu_class_inheritance/class_inheritance.rb
+++ mthunzi_chikhungu_class_inheritance/class_inheritance.rb
@@ -1,7 +1,22 @@
 class Employee
+    attr_reader :name, :title, :boss
+    attr_writer :salary
+
+    def initialize(name, title, salary, boss = nil)
+        @name, @title, @salary, @boss = name, title, salary, boss
+    end
+
+    def salary
+        "$#{@salary}"
+    end
 
 end
 
 class Manager < Employee
 
 end
+
+
+# ned.bonus(5) # => 500_000
+# darren.bonus(4) # => 88_000
+# david.bonus(3) # => 30_000
