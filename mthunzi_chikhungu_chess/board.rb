d397cd1678984980d7e783e3db1b9b3c15ee193b
Mthunzi <mthunzi99@gmail.com>
2022-11-16 00:45:37 +0200

    Starting Phase 2

--- mthunzi_chikhungu_chess/board.rb
+++ mthunzi_chikhungu_chess/board.rb
@@ -8,7 +8,7 @@ class Board
         (0...8).each do |row|
             (0...8).each do |col|
                 if piece_rows.include?(row)
-                    @rows[row][col] = Piece.new 
+                    @rows[row][col] = Piece.new(self)
                 else
                     @rows[row][col] = NullPiece.new
                 end
@@ -22,7 +22,7 @@ class Board
         @rows = Array.new(8) { Array.new(8) }
         @null_piece = NullPiece.new
 
-        # populate_board
+        populate_board
     end
 
     def [](pos)
@@ -35,6 +35,12 @@ class Board
         @rows[x][y] = val
     end
 
+    def add_piece(piece, pos)
+        raise "This position is not empty" unless self[pos].empty?
+
+        self[pos] = piece
+    end
+
     def move_piece(start_pos, end_pos)
         raise ArgumentError.new "There is no piece at #{start_pos}" if self[start_pos].is_a?(NullPiece)
         raise ArgumentError.new "You cannot move to #{end_pos}" if self[end_pos].is_a?(Piece)
@@ -45,5 +51,28 @@ class Board
         nil
     end
 
+    def valid_pos?(pos)
+
+    end
+
+    def checkmate?(colour)
+
+    end
+
+    def in_check?(colour)
+
+    end
+
+    def find_king(colour)
+
+    end
+
+    def pieces
+
+    end
+
+    def dup
+        @rows.dup
+    end
 
 end
--- mthunzi_chikhungu_chess/piece.rb
+++ mthunzi_chikhungu_chess/piece.rb
@@ -1,7 +1,31 @@
 class Piece
+    attr_reader :colour, :board
+    attr_accessor :pos
 
-    def initialize
+    def initialize(colour, board, pos)
+        @colour, @board, @pos = colour, board, pos
 
+        board.add_piece(self)
+    end
+
+    def to_s
+        " #{symbol} "
+    end
+
+    def empty?
+        false
+    end
+
+    def valid_moves
+        moves.reject! { |move| move_into_check?(move) }
+    end
+
+    private
+
+    def move_into_check?(end_pos)
+        dup_board = board.dup
+        dup_board.move_piece(@pos, end_pos)
+        
     end
 
 end
