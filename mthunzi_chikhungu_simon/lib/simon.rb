876d5f7990311858fc036250d6ee080fb70cf82c
Mthunzi <mthunzi99@gmail.com>
2023-01-26 00:58:53 +0200

    All specs completed

--- mthunzi_chikhungu_simon/lib/simon.rb
+++ mthunzi_chikhungu_simon/lib/simon.rb
@@ -4,19 +4,31 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
+    @sequence_length, @game_over, @seq = 1, false, []
 
   end
 
   def play
+    until @game_over
+      take_turn
+    end
 
+    game_over_message
+    reset_game
   end
 
   def take_turn
+    show_sequence
+    require_sequence
 
+    return if @game_over
+
+    round_success_message 
+    @sequence_length += 1
   end
 
   def show_sequence
-
+    add_random_color
   end
 
   def require_sequence
@@ -24,7 +36,7 @@ class Simon
   end
 
   def add_random_color
-
+    @seq << COLORS.sample
   end
 
   def round_success_message
@@ -36,6 +48,6 @@ class Simon
   end
 
   def reset_game
-
+    @sequence_length, @game_over, @seq = 1, false, []
   end
 end
