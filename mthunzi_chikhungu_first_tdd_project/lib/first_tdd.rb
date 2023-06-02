1e71c82c8b865804905ec9773c5a2ab093e55427
mthunzi99 <mthunzi99@gmail.com>
2023-06-02 23:51:29 +0200

    first_tdd project started

---
+++ mthunzi_chikhungu_first_tdd_project/lib/first_tdd.rb
@@ -0,0 +1,24 @@
+class Array
+  def uniq
+
+  end
+
+  def two_sum
+
+  end 
+end
+
+def my_transpose(matrix)
+  
+end
+
+def stock_picker(stock_prices)
+  
+end
+
+class TowersOfHanoi
+  def initialize
+
+  end
+
+end
---
+++ mthunzi_chikhungu_first_tdd_project/spec/first_tdd_spec.rb
@@ -0,0 +1,13 @@
+require 'first_tdd'
+require 'rspec'
+
+describe Array do
+  subject(:array) { Array.new }
+
+  describe "#uniq" do
+    it "removes duplicates from an array." do
+      array += [1, 2, 1, 3, 3]
+      expect(array.uniq).to eq([1, 2, 3])
+    end
+  end
+end
