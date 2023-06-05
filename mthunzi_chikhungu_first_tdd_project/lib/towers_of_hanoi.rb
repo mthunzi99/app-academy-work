4bd6343d9190de74aac98144a4ac53fd1dfd5a65
mthunzi99 <mthunzi99@gmail.com>
2023-06-06 00:16:30 +0200

    all specs completed and cleared

--- mthunzi_chikhungu_first_tdd_project/lib/towers_of_hanoi.rb
+++ mthunzi_chikhungu_first_tdd_project/lib/towers_of_hanoi.rb
@@ -1,6 +1,34 @@
 class TowersOfHanoi
+  attr_reader :stacks
   def initialize
-
+    @stacks = [[3,2,1,], [], []]
   end
 
+  def play
+    
+  end
+
+  def render
+    "Tower 1: #{@stacks[0].join("  ")} " + "\n" +
+    "Tower 2: #{@stacks[1].join("  ")} " + "\n" +
+    "Tower 3: #{@stacks[2].join("  ")} "
+  end
+
+  def move(from, to)
+    raise ArgumentError.new "You can only use the numbers 1, 2, and 3" unless [from, to].all? { |i| i.between?(1, 3) }
+    from_tower, to_tower = from-1, to-1
+    raise "Cannot move from an empty tower" if @stacks[from_tower].empty?
+    raise "Cannot move disk onto smaller disk" unless valid_move?(from_tower, to_tower)
+    
+    @stacks[to_tower] << @stacks[from_tower].pop
+  end
+
+  def valid_move?(from, to)
+    !@stacks[from].empty? && (@stacks[to].empty? || @stacks[to].last > @stacks[from].last)
+  end
+
+  def won?
+    @stacks[1] == [3,2,1] || @stacks[2] == [3,2,1]
+  end
 end
+
--- mthunzi_chikhungu_first_tdd_project/spec/towers_of_hanoi_spec.rb
+++ mthunzi_chikhungu_first_tdd_project/spec/towers_of_hanoi_spec.rb
@@ -2,5 +2,68 @@ require 'towers_of_hanoi'
 require 'rspec'
 
 describe TowersOfHanoi do
+  subject(:towers) { TowersOfHanoi.new }
 
+  describe "#initialize" do
+    it "sets 3 towers with the first tower having 3 disk" do
+      expect(towers.stacks).to eq([[3,2,1], [], []])
+    end
+  end
+
+  describe "#render" do
+    it "displays the towers" do
+      expect(towers.render).to eq("Tower 1: 3  2  1 \nTower 2:  \nTower 3:  ")
+    end
+  end
+
+  describe "#move" do 
+    it "allows a disk to move from one tower to another" do
+      expect { towers.move(1,2) }.to_not raise_error
+    end
+
+    it "allows a smaller disk to move onto a bigger disk" do
+      towers.move(1,2)
+      towers.move(1,3)
+      expect { towers.move(2,3) }.to_not raise_error
+    end
+
+    it "does not allow bigger disk to move onto smaller disk" do
+      towers.move(1,2)
+      expect { towers.move(1,2) }.to raise_error("Cannot move disk onto smaller disk")
+    end
+
+    it "does not allow moving from an empty tower" do
+      expect {towers.move(2,1) }.to raise_error("Cannot move from an empty tower")
+    end
+  end
+
+  describe "#won" do
+    it "returns false when the game hasn't been won" do
+      expect(towers).to_not be_won
+    end
+
+    it "returns true when all the disks are on the second tower" do
+      towers.move(1, 2)
+      towers.move(1, 3)
+      towers.move(2, 3)
+      towers.move(1, 2)
+      towers.move(3, 1)
+      towers.move(3, 2)
+      towers.move(1, 2)
+
+      expect(towers).to be_won
+    end
+
+    it "returns true when all the disks are on the third tower" do
+      towers.move(1, 3)
+      towers.move(1, 2)
+      towers.move(3, 2)
+      towers.move(1, 3)
+      towers.move(2, 1)
+      towers.move(2, 3)
+      towers.move(1, 3)
+
+      expect(towers).to be_won
+    end
+  end
 end
